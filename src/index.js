fd53228535b8b6e3f1fb84467284e70a46fbb080
david <ostermann.david@gmail.com>
2016-12-29 01:03:02 +0100

    probleme react dev tools

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+/** /
+// Bizarre : React dev tools ne se lance pas
+if (typeof window !== 'undefined') {
+    window.React = React;
+}
+/**/
 
 ReactDOM.render(
   <App />,
